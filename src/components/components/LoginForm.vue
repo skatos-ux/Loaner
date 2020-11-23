d37251a93301e328fe311ac87cb650badba37386
skatos-ux <nicolas.magne1@outlook.fr>
2020-11-23 16:05:25 +0100

    feat: libraries

---
+++ src/components/components/LoginForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class LoginForm extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 import router from './router'
 import store from './store'
+
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(fas)
+
+// Bulma
+import './../node_modules/bulma/css/bulma.css';
+
+//Axios
+Vue.use({
+  install (Vue) {
+    Vue.prototype.$api = axios.create({
+      baseURL: 'http://localhost:3000/'
+    })
+  }
+})
 
 Vue.config.productionTip = false
 
