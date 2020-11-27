cea88177ae789c3410bb3e84b6385fe9a8598073
skatos-ux <nicolas.magne1@outlook.fr>
2020-11-27 17:56:29 +0100

    design: DeviceCategoryLayout

---
+++ src/components/layouts/DeviceCategoryLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="deviceCategory has-text-left">
+    <h5 class="title is-5">{{ title }}</h5>
+    <div class="devicesCategory__container">
+      <!-- TODO: implement slots -->
+      <Device/>
+      <Device/>
+      <Device/>
+      <Device/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Device from "@/components/components/Device.vue";
+@Component({
+  components: {Device}
+})
+export default class DeviceCategoryLayout extends Vue {
+  @Prop() private title!: string;
+}
+</script>
+
+<style scoped lang="scss">
+@import "./../../scss/globals";
+  .deviceCategory {
+    background-color: red;
+    padding: 1.25rem;
+    &--container {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+    }
+  }
+</style>
