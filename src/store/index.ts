a050a5bbacf88337badb250764a06950bffe71fd
skatos-ux <nicolas.magne1@outlook.fr>
2020-11-30 00:59:53 +0100

    update: user implementation

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { auth } from './modules/auth'
 
 Vue.use(Vuex)
 
@@ -73,5 +74,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    auth
   }
 })
