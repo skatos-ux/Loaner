f3cdc6c79f32030a531a54da6e0343e02cd72b73
skatos-ux <nicolas.magne1@outlook.fr>
2020-12-08 13:56:19 +0100

    feat: CartItem

---
+++ src/components/components/CartItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    <a class="control has-icons-right fail--hover">
+      {{ name }}
+      <span @click="remFromCart" class="icon is-small is-right test">
+        <font-awesome-icon :icon="['fas', 'trash-alt']" />
+      </span>
+    </a>
+  </li>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CartItem extends Vue {
+  @Prop() private identifier!: number;
+  @Prop() private name!: string;
+  @Prop() private version!: string;
+  @Prop() private reference!: string;
+  @Prop() private loanStart!: number;
+  @Prop() private loanEnd!: number;
+
+  item = {
+    identifier: this.identifier,
+    name: this.name,
+    version: this.version,
+    reference: this.reference,
+    loanStart: this.loanStart,
+    loanEnd: this.loanEnd
+  }
+  remFromCart(event: { target: HTMLInputElement}) {
+    this.$store.dispatch('remFromCart', this.item)
+  }
+
+
+}
+</script>
+
+<style scoped lang="scss">
+  @import "./../../scss/globals";
+</style>
