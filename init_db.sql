37f2b041e54d9bdd8da41ed7dd7889c95e1dec2b
Easycash <easycash@MacBook-Pro-de-Milan.local>
2020-12-26 20:01:39 +0100

    ajout delete,add, getall categories

--- init_db.sql
+++ init_db.sql
@@ -6,12 +6,12 @@ DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS reservation;
 
 
-CREATE TABLE device(ref CHAR(5), idCategory INTEGER, name VARCHAR(30), version VARCHAR(15), photo VARCHAR, phone INTEGER(10));
+CREATE TABLE device(ref CHAR(5), idCategory INTEGER, name VARCHAR(30), version VARCHAR(15), photo VARCHAR, phone VARCHAR(10));
 CREATE TABLE user(id CHAR(7), firstName VARCHAR(30), lastName VARCHAR(15), mail VARCHAR(255), admin INTEGER(1), password VARCHAR(255));
-CREATE TABLE category(id INTEGER, name INTEGER);
+CREATE TABLE category(id INTEGER, name VARCHAR(255));
 CREATE TABLE reservation(id INTEGER PRIMARY KEY, refDevice CHAR(5), idUser CHAR(7), startDate DATETIME, endDate DATETIME, returnDate DATETIME);
 -- A completer
 
-INSERT INTO device VALUES("AN001", 1, "Samsung Galaxy S1000", "1.0", "", 0123456789);
+INSERT INTO device VALUES("AN001", 1, "Samsung Galaxy S1000", "1.0", "", "0123456789");
 
 INSERT INTO user VALUES("0", "Lilian", "Bethus", "lilianb@mail.fr", 1, "fromage");
--- src/controller/controller_device.ts
+++ src/controller/controller_device.ts
@@ -2,6 +2,7 @@ import Controller from './controller';
 import DAODevice from '../dao/dao_device';
 
 import { Response } from 'express';
+import Device from '../model/device';
 
 export default class DeviceController extends Controller {
 
@@ -18,5 +19,9 @@ export default class DeviceController extends Controller {
 
     public async borrowDevice(res : Response, idDevice : String, idUser : String) : Promise<void> {
         this.dao.borrowDevice(idDevice, idUser).then(this.findSuccess(res)).catch(this.findError(res));
+    }
+
+    public async addDevice(res: Response, device : Device) : Promise<void> {
+        this.dao.addDevice(device).then(this.findSuccess(res)).catch(this.findError(res));
     }
 }
