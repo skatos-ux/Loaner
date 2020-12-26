2b8ab30c5f6d71da54e75d631097d51a153cf1d6
skatos-ux <nicolas.magne1@outlook.fr>
2020-12-26 10:33:43 +0100

    update: Mainpage router

---
+++ src/views/MainPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="mainpage">
+    <div v-if="isAdmin" class="mainpage__header">
+      <NavBar/>
+    </div>
+    <div :class="'mainpage__body' + adminLayout()">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import NavBar from "@/components/components/NavBar.vue";
+
+@Component({
+  components: { NavBar }
+})
+export default class MainPage extends Vue {
+  user = this.$store.state.auth.user
+
+  mounted() {
+    if(!this.user.logged) {
+      this.$router.push("/")
+    }
+  }
+
+  isAdmin(){
+    return this.user.rank === 0
+  }
+  adminLayout() {
+    if(this.isAdmin()){
+      return "--admin"
+    } else {
+      return ""
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../scss/globals";
+  .mainpage {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    &__header {
+      position: relative;
+      z-index: 1;
+    }
+    &__body--admin {
+      position: relative;
+      width: 100%;
+      height: calc(100% - #{$navbar-height} - #{$dashboard-margin * 2});
+    }
+  }
+</style>
