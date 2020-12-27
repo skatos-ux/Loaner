24665135bf091315093c9f7ae76b875be1f729a1
Lafont <paullafont@orange.fr>
2020-12-27 18:29:32 +0100

    Ajout fichier tests

---
+++ tests/daotests/dao_device_test.ts
@@ -0,0 +1,13 @@
+import DAODevice from '../../src/dao/dao_device';
+
+const assert = require('chai').assert;
+
+/* TODO:
+- Vérifier que les types des variabes soient du même type que les attributs de la classe;
+- Vérifier que chaque appareil appartiennent à une categorie existante
+- Vérifier que chaque appareil ait une reference non nulle
+- vérifier que chaque appareil ait un nom non null
+- Vérifier qu'une version soit inscrite entre 3 et 15 caractères
+- Vérifier que chaque version est supérieure à 0.0
+- Vérifier
+*/
