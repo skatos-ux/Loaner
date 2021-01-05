97faaf8d172d2a9421eabbfd7910cd0ca68fdade
Yohan Rousseau <rousseauyohan2@gmail.com>
2021-01-05 20:02:03 +0100

    Ajout des contraintes NOT NULL dans la base de données

--- init_db.sql
+++ init_db.sql
@@ -8,12 +8,17 @@ DROP TABLE IF EXISTS device;
 DROP TABLE IF EXISTS category;
 
 -- Création des tables
-CREATE TABLE user(id CHAR(7) PRIMARY KEY, firstName VARCHAR(30), lastName VARCHAR(30), mail VARCHAR(255), admin INTEGER(1),
-    password VARCHAR(255), temporaryPassword INTEGER(1));
-CREATE TABLE category(id INTEGER PRIMARY KEY , name VARCHAR(255));
-CREATE TABLE device(ref CHAR(5) PRIMARY KEY, idCategory INTEGER, name VARCHAR(30), version VARCHAR(15), photo VARCHAR, phone VARCHAR(12),
+CREATE TABLE user(id CHAR(7) NOT NULL PRIMARY KEY, firstName VARCHAR(30) NOT NULL, lastName VARCHAR(30) NOT NULL, mail VARCHAR(255),
+    admin INTEGER(1) NOT NULL, password VARCHAR(255) NOT NULL, temporaryPassword INTEGER(1) NOT NULL);
+
+CREATE TABLE category(id INTEGER NOT NULL PRIMARY KEY , name VARCHAR(255) NOT NULL);
+
+CREATE TABLE device(ref CHAR(5) NOT NULL PRIMARY KEY, idCategory INTEGER NOT NULL, name VARCHAR(30) NOT NULL, version VARCHAR(15) NOT NULL,
+    photo VARCHAR, phone VARCHAR(12),
     FOREIGN KEY (idCategory) REFERENCES category(id));
-CREATE TABLE reservation(id INTEGER PRIMARY KEY, refDevice CHAR(5), idUser CHAR(7), startDate DATETIME, endDate DATETIME, returnDate DATETIME,
+
+CREATE TABLE reservation(id INTEGER NOT NULL PRIMARY KEY, refDevice CHAR(5) NOT NULL, idUser CHAR(7) NOT NULL, startDate DATETIME NOT NULL,
+    endDate DATETIME, returnDate DATETIME,
     FOREIGN KEY (refDevice) REFERENCES device(ref),
     FOREIGN KEY (idUser) REFERENCES user(id));
 
